92799a2d565d979dadc8aeb3bd51f6c4bb57c610
MingzhaoLiang <81400833+MingzhaoLiang@users.noreply.github.com>
2023-01-09 22:16:55 +1100

    Update settings.py
    update backend

--- docker/django_docker_app/code_review_mining/settings.py
+++ docker/django_docker_app/code_review_mining/settings.py
@@ -150,7 +150,11 @@ BASE_URL = 'https://127.0.0.1:8000/'
 
 # Enable filter backend
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 30
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend', 
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50
 }
