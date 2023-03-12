65c17f58036b38132833485b7ce8030f74e152f6
MingzhaoLiang <81400833+MingzhaoLiang@users.noreply.github.com>
2023-03-13 07:37:40 +1100

    Update models.py
    1. Change model name (plural -> single, account -> identity, user -> individual)
    2. include foreign key in models
    3. create two through model for relation between project and identity and the one between individual and identity
    4. delete submitter and series related field in some models
    5. change some field names

--- docker/django_docker_app/patchwork/models.py
+++ docker/django_docker_app/patchwork/models.py
@@ -5,21 +5,17 @@ from djongo.storage import GridFSStorage
 
 grid_fs_storage = GridFSStorage(collection='textfiles', base_url=''.join([settings.BASE_URL, 'textfiles/']))
 
+
 # Create your models here.
-class Accounts(models.Model):
+class Identity(models.Model):
     original_id = models.CharField(max_length=255, unique=True)
     email = models.CharField(max_length=255, blank=True, null=True)
-    username = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
     api_url = models.CharField(max_length=255, blank=True, null=True)
-    user_original_id = models.CharField(max_length=255, blank=True, null=True)
-    
-    
-class Users(models.Model):
-    original_id = models.CharField(max_length=255, unique=True)
-    account_original_id = models.JSONField(blank=True, null=True)
-    
+    is_maintainer = models.BooleanField(default=False)
 
-class Projects(models.Model):
+
+class Project(models.Model):
     original_id = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255, blank=True, null=True)
     repository_url = models.CharField(max_length=255, blank=True, null=True)
@@ -27,11 +23,25 @@ class Projects(models.Model):
     web_url = models.CharField(max_length=255, blank=True, null=True)
     list_id = models.CharField(max_length=255, blank=True, null=True)
     list_address = models.CharField(max_length=255, blank=True, null=True)
-    
-    maintainer_account_original_id = models.JSONField(blank=True, null=True)
-    maintainer_user_original_id = models.JSONField(blank=True, null=True)
-    
-    
+    maintainer_identity = models.ManyToManyField(Identity, through="ProjectIdentityRelation", blank=True, null=True)
+
+
+class ProjectIdentityRelation(models.Model):
+    project_original_id = models.ForeignKey(Project, on_delete=models.CASCADE, to_field="original_id", db_column="project_original_id")
+    identity_original_id = models.ForeignKey(Identity, on_delete=models.CASCADE, to_field="original_id", db_column="identity_original_id")
+
+
+class Individual(models.Model):
+    original_id = models.CharField(max_length=255, unique=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, to_field="original_id", db_column="project", blank=True, null=True)
+    identity = models.ManyToManyField(Identity, through="IndividualIdentityRelation", related_name="individuals", blank=True, null=True)
+
+
+class IndividualIdentityRelation(models.Model):
+    individual_original_id = models.ForeignKey(Individual, on_delete=models.CASCADE, to_field="original_id", db_column="individual_original_id")
+    identity_original_id = models.ForeignKey(Identity, on_delete=models.CASCADE, to_field="original_id", db_column="identity_original_id")
+
+
 class Series(models.Model):
     original_id = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255, blank=True, null=True)
@@ -44,55 +54,54 @@ class Series(models.Model):
     api_url = models.CharField(max_length=255, blank=True, null=True)
     web_url = models.CharField(max_length=255, blank=True, null=True)
     
-    project_original_id = models.CharField(max_length=255, blank=True, null=True)
-    submitter_account_original_id = models.CharField(max_length=255, blank=True, null=True)
-    submitter_user_original_id = models.CharField(max_length=255, blank=True, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True, to_field="original_id", db_column="project")
+    submitter_identity = models.ForeignKey(Identity, on_delete=models.RESTRICT, blank=True, null=True, to_field="original_id", db_column="submitter_identity")
+    submitter_individual = models.ForeignKey(Individual, on_delete=models.RESTRICT, blank=True, null=True, to_field="original_id", db_column="submitter_individual")
+    # newseries = models.ForeignKey(NewSeries, on_delete=models.DO_NOTHING, blank=True, null=True)
 
 
 class NewSeries(models.Model):
     original_id = models.CharField(max_length=255, unique=True)
     cover_letter_msg_id = models.TextField(blank=True, null=True)
 
-    project_original_id = models.CharField(max_length=255, blank=True, null=True)
-    submitter_account_original_id = models.JSONField(blank=True, null=True)
-    submitter_user_original_id = models.JSONField(blank=True, null=True)
-    series_original_id = models.JSONField(blank=True, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True, to_field="original_id", db_column="project")
+    # submitter_identity = models.ManyToManyField(Identity, blank=True)
+    # submitter_individual = models.ManyToManyField(Individual, blank=True)
+    # series = models.ManyToManyField(Series, blank=True)
     inspection_needed = models.BooleanField(default=False)
 
     
-class Changes1(models.Model):
+class Change1(models.Model):
     original_id = models.CharField(max_length=255, unique=True)
     is_accepted = models.BooleanField(default=False)
     parent_commit_id = models.TextField(blank=True, null=True)
     merged_commit_id = models.TextField(blank=True, null=True)
     commit_date = models.DateTimeField(blank=True, null=True)
     
-    project_original_id = models.CharField(max_length=255, blank=True, null=True)
-    submitter_account_original_id = models.JSONField(blank=True, null=True)
-    submitter_user_original_id = models.JSONField(blank=True, null=True)
-    series_original_id = models.JSONField(blank=True, null=True)
-    new_series_original_id = models.JSONField(blank=True, null=True)
-    patch_original_id = models.JSONField(blank=True, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True, to_field="original_id", db_column="project")
+    # submitter_identity = models.ManyToManyField(Identity, blank=True, null=True)
+    # submitter_individual = models.ManyToManyField(Individual, blank=True, null=True)
+    # series = models.ManyToManyField(Series, blank=True, null=True)
+    # newseries = models.ManyToManyField(NewSeries, blank=True, null=True)
     inspection_needed = models.BooleanField(default=False)
 
 
-class Changes2(models.Model):
+class Change2(models.Model):
     original_id = models.CharField(max_length=255, unique=True)
     is_accepted = models.BooleanField(default=False)
     parent_commit_id = models.TextField(blank=True, null=True)
     merged_commit_id = models.TextField(blank=True, null=True)
     commit_date = models.DateTimeField(blank=True, null=True)
     
-    project_original_id = models.CharField(max_length=255, blank=True, null=True)
-    submitter_account_original_id = models.JSONField(blank=True, null=True)
-    submitter_user_original_id = models.JSONField(blank=True, null=True)
-    series_original_id = models.JSONField(blank=True, null=True)
-    new_series_original_id = models.JSONField(blank=True, null=True)
-    patch_original_id = models.JSONField(blank=True, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True, to_field="original_id", db_column="project")
+    # submitter_identity = models.ManyToManyField(Identity, blank=True)
+    # submitter_individual = models.ManyToManyField(Individual, blank=True)
+    # series = models.ManyToManyField(Series, blank=True)
+    # newseries = models.ManyToManyField(NewSeries, blank=True)
     inspection_needed = models.BooleanField(default=False)
 
     
-class MailingLists(models.Model):
+class MailingList(models.Model):
     original_id = models.CharField(max_length=255, unique=True)
     msg_id = models.TextField(unique=True, blank=True, null=True)
     subject = models.TextField(blank=True, null=True)
@@ -100,10 +109,10 @@ class MailingLists(models.Model):
     date = models.DateTimeField(blank=True, null=True)
     sender_name = models.CharField(max_length=255, blank=True, null=True)
     web_url = models.CharField(max_length=255, blank=True, null=True)
-    project_original_id = models.CharField(max_length=255, blank=True, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True, to_field="original_id", db_column="project")
 
     
-class Patches(models.Model):
+class Patch(models.Model):
     original_id = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255, blank=True, null=True)
     state = models.CharField(max_length=255, blank=True, null=True)
@@ -114,31 +123,31 @@ class Patches(models.Model):
     api_url = models.CharField(max_length=255, blank=True, null=True)
     web_url = models.CharField(max_length=255, blank=True, null=True)
     commit_ref = models.TextField(blank=True, null=True)
-    reply_to_msg_id = models.TextField(blank=True, null=True)
+    in_reply_to = models.TextField(blank=True, null=True)
     
-    change1_original_id = models.CharField(max_length=255, blank=True, null=True)
-    change2_original_id = models.CharField(max_length=255, blank=True, null=True)
-    mailing_list_original_id = models.CharField(max_length=255, blank=True, null=True)
-    series_original_id = models.CharField(max_length=255, blank=True, null=True)
-    new_series_original_id = models.CharField(max_length=255, blank=True, null=True)
-    submitter_account_original_id = models.CharField(max_length=255, blank=True, null=True)
-    submitter_user_original_id = models.CharField(max_length=255, blank=True, null=True)
-    project_original_id = models.CharField(max_length=255, blank=True, null=True)
+    change1 = models.ForeignKey(Change1, related_name='patches', on_delete=models.RESTRICT, blank=True, null=True, to_field="original_id", db_column="change1")
+    change2 = models.ForeignKey(Change2, related_name='patches', on_delete=models.RESTRICT, blank=True, null=True, to_field="original_id", db_column="change2")
+    mailinglist = models.ForeignKey(MailingList, related_name='patches', on_delete=models.RESTRICT, blank=True, null=True, to_field="original_id", db_column="mailinglist")
+    series = models.ForeignKey(Series, related_name='patches', on_delete=models.RESTRICT, blank=True, null=True, to_field="original_id", db_column="series")
+    newseries = models.ForeignKey(NewSeries, related_name='patches', on_delete=models.RESTRICT, blank=True, null=True, to_field="original_id", db_column="newseries")
+    submitter_identity = models.ForeignKey(Identity, on_delete=models.RESTRICT, blank=True, null=True, to_field="original_id", db_column="submitter_identity")
+    submitter_individual = models.ForeignKey(Individual, on_delete=models.RESTRICT, blank=True, null=True, to_field="original_id", db_column="submitter_individual")
+    project = models.ForeignKey(Project, on_delete=models.RESTRICT, blank=True, null=True, to_field="original_id", db_column="project")
 
     
-class Comments(models.Model):
+class Comment(models.Model):
     original_id = models.CharField(max_length=255, unique=True)
     msg_id = models.TextField(blank=True, null=True)
     msg_content = models.FileField(blank=True, null=True, upload_to='comment_msg_content', storage=grid_fs_storage)
     date = models.DateTimeField(blank=True, null=True)
     subject = models.TextField(blank=True, null=True)
-    reply_to_msg_id = models.TextField(blank=True, null=True)
+    in_reply_to = models.TextField(blank=True, null=True)
     web_url = models.CharField(max_length=255, blank=True, null=True)
     
-    change1_original_id = models.CharField(max_length=255, blank=True, null=True)
-    change2_original_id = models.CharField(max_length=255, blank=True, null=True)
-    mailing_list_original_id = models.CharField(max_length=255, blank=True, null=True)
-    submitter_account_original_id = models.CharField(max_length=255, blank=True, null=True)
-    submitter_user_original_id = models.CharField(max_length=255, blank=True, null=True)
-    patch_original_id = models.CharField(max_length=255, blank=True, null=True)
-    project_original_id = models.CharField(max_length=255, blank=True, null=True)
+    change1 = models.ForeignKey(Change1, related_name='comments', on_delete=models.RESTRICT, blank=True, null=True, to_field="original_id", db_column="change1")
+    change2 = models.ForeignKey(Change2, related_name='comments', on_delete=models.RESTRICT, blank=True, null=True, to_field="original_id", db_column="change2")
+    mailinglist = models.ForeignKey(MailingList, related_name='comments', on_delete=models.DO_NOTHING, blank=True, null=True, to_field="original_id", db_column="mailinglist")
+    submitter_identity = models.ForeignKey(Identity, on_delete=models.RESTRICT, blank=True, null=True, to_field="original_id", db_column="submitter_identity")
+    submitter_individual = models.ForeignKey(Individual, on_delete=models.RESTRICT, blank=True, null=True, to_field="original_id", db_column="submitter_individual")
+    patch = models.ForeignKey(Patch, related_name='comments', on_delete=models.CASCADE, blank=True, null=True, to_field="original_id", db_column="patch")
+    project = models.ForeignKey(Project, on_delete=models.RESTRICT, blank=True, null=True, to_field="original_id", db_column="project")
